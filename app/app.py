a69d9c541f197cc9462b826b9a4d108d687242ac
ikonomov17 <aleksandar.ikonomov@abv.bg>
2017-05-18 21:23:18 +0300

    Added data to quiz table database and linked it to html

--- app/app.py
+++ app/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_config import Base, Quiz, Question, Answer
 app = Flask(__name__)
 
+
+engine = create_engine('sqlite:///quiz.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def HomePage():
+	quizzes = session.query(Quiz).all();
+	for u in quizzes:
+		print u.__dict__
+	return render_template('index.html', quizzes = quizzes)
 
 
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ app/database_config.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Quiz(Base):
+	__tablename__ = 'quizzes'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(50), nullable=False)
+	programming_language = Column(String(50), nullable=False)
+	message = Column(String(250), nullable = False)
+
+class Question(Base):
+	__tablename__ = 'questions'
+
+	id = Column(Integer, primary_key=True)
+	text = Column(String(500), nullable=False)
+	quiz_id = Column(Integer, ForeignKey('quizzes.id'))
+	quiz = relationship(Quiz)
+
+class Answer(Base):
+	__tablename__ = 'answers'
+
+	id = Column(Integer, primary_key=True)
+	text = Column(String(500), nullable=False)
+	question_id = Column(Integer, ForeignKey('questions.id'))
+	quiz = relationship(Question)
+
+engine = create_engine('sqlite:///quiz.db')
+
+
+Base.metadata.create_all(engine)
