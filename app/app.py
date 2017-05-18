a08e926b11196e765719f4c4094441c589ac50c9
ikonomov17 <aleksandar.ikonomov@abv.bg>
2017-05-19 00:00:53 +0300

    Added working quiz page.Shuffling answers.

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_config import Base, Quiz, Question, Answer
+import random
+from random import shuffle
 app = Flask(__name__)
 
 
@@ -18,6 +20,19 @@ def HomePage():
 	for u in quizzes:
 		print u.__dict__
 	return render_template('index.html', quizzes = quizzes)
+
+@app.route('/python')
+def PythonQuiz():
+	question = session.query(Question).first()
+	correctAnswer = session.query(Answer).get(question.id)
+	query = session.query(Answer)
+	rowCount = int(query.count())
+	firstRandomRow = query.offset(int(rowCount*random.random())).first()
+	secondRandomRow = query.offset(int(rowCount*random.random())).first()
+
+	allAnswers = [firstRandomRow,secondRandomRow,correctAnswer]
+	shuffle(allAnswers)
+	return render_template('quizsheet.html', question = question, answers = allAnswers)
 
 
 if __name__ == '__main__':
