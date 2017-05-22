b8203b739c5710383bac1899419f9d47fe3f3699
ikonomov17 <aleksandar.ikonomov@abv.bg>
2017-05-22 10:39:04 +0300

    Added defense clauses for wrong urls

--- app/app.py
+++ app/app.py
@@ -13,27 +13,29 @@ Base.metadata.bind = ENGINE
 DBSession = sessionmaker(bind=ENGINE)
 session = DBSession()
 
-first_session = scoped_session(sessionmaker(bind=ENGINE))
-first_questions = dict();
-quizzes = first_session.query(Quiz).order_by(Quiz.id).all()
-for quiz in quizzes:
-        first_question_id = first_session.query(Question).filter(Question.quiz_id == quiz.id).first().id
-        first_questions.update({quiz.programming_language: first_question_id})
-
-@app.route('/')
-def HomePage():
+def GetLanguagesWithFirstQuestionId():
+    first_questions = dict()
     quizzes = session.query(Quiz).order_by(Quiz.id).all()
-
-    data = dict()
 
     for quiz in quizzes:
         first_question_id = session.query(Question).filter(Question.quiz_id == quiz.id).first().id
-        first_questions.update({quiz.programming_language: first_question_id})
-        data.update({quiz :first_question_id})
+        first_questions.update({quiz: first_question_id})
+
+    return first_questions
+
+def IsLanguageInDatabase(lang, db):
+    for record in db:
+        if(record.programming_language == lang):
+            return True
+    return False
+
+@app.route('/')
+def HomePage():
+    data = GetLanguagesWithFirstQuestionId()
 
     return render_template('index.html', data=data)
 
-@app.route('/<lang>/add_user', methods=['GET','POST'])
+@app.route('/<string:lang>/add_user', methods=['GET','POST'])
 def AddUser(lang):
     if request.method == 'GET':
         return render_template('proba.html')
@@ -48,19 +50,25 @@ def AddUser(lang):
         current_user = User(request.form['name'], user_answers_id, user_questions_id)
         session.add(current_user)
         session.commit()
+        first_questions = GetLanguagesWithFirstQuestionId()
         return redirect(url_for('QuizResponse', lang = lang, question_id = first_questions[lang]))
 
-@app.route('/<lang>')
-@app.route('/<lang>/')
-def RedirectToFirst(lang):
-    return redirect(url_for('QuizResponse', lang = lang, question_id = first_questions[lang]))
+@app.route('/<string:lang>')
+@app.route('/<string:lang>/')
+def RedirectToAddUser(lang):
+    first_questions = GetLanguagesWithFirstQuestionId()
+    LangIsInDict = IsLanguageInDatabase(lang, first_questions) 
+    if LangIsInDict:
+        return redirect(url_for('AddUser', lang = lang))
+    else:
+        return render_template('error404.html',message='No such language :))')
 
-@app.route('/<lang>/<int:question_id>')
+@app.route('/<string:lang>/<int:question_id>')
 def QuizResponse(lang, question_id):
     current_language = session.query(Quiz).filter(Quiz.programming_language == lang).first()
     # check if there is such language in database, if not should return error page
     if current_language is None:
-        return render_template('questions.html',programming_language='no such language')
+        return render_template('error404.html',message='No such language :))')
 
     current_language_id = current_language.id
     questions = session.query(Question).filter(Question.quiz_id == current_language_id).all()
@@ -73,7 +81,7 @@ def QuizResponse(lang, question_id):
     # check if the question_id passed in url is valid for there lang
     question_id = int(question_id)
     if question_id not in all_questions_ids:
-      return render_template('proba.html',programming_language='no such question for this lang')
+      return render_template('error404.html',message='No question with that id for this quiz')
 
     answers = session.query(Answer).filter(Answer.question_id.in_(all_questions_ids))
     exact_question = session.query(Question).get(question_id)
